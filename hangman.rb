14aa49f53270774a1ee38d589443a078000eaf8a
Dan Molloy <danieljmolloy1@gmail.com>
2015-01-20 17:43:08 +0000

    saving and loading

--- hangman.rb
+++ hangman.rb
@@ -1,13 +1,71 @@
-class Hangman
-  attr_reader :word
+require 'yaml'
 
+class Hangman
   def initialize
+    @save_dir = "saved_games"
+    Dir.mkdir(@save_dir) unless File.exists?(@save_dir)
     @word = pick_word
     @guesses_remaining = 16
     @guesses = []
     @solution = Array.new(@word.length, "_")
     @victory_state = "none"
-    play
+    welcome_screen
+  end
+
+  def welcome_screen
+    puts "Would you like to:"
+    puts "\t1 - Start a new game"
+    puts "\t2 - Load a saved game"
+    choice = gets.chomp
+    if choice == '1'
+      play
+    elsif choice == '2'
+      load_game
+    else
+      puts "Invalid option"
+      welcome_screen
+    end
+  end
+
+  def save_game
+    puts "Enter a name for your save file: "
+    save_name = @save_dir + "/" + gets.chomp + ".save"
+    if File.exists?(save_name)
+      puts "Save file with that name already exists. Do you wish to"
+      puts "overwrite? (yes/no)"
+      choice = gets.chomp
+      save_game unless choice == "yes" || choice == "y"
+    end
+    puts 'saving...'
+    save_file = File.open(save_name, "w")
+    save_data = self
+    save_file.puts YAML::dump(save_data)
+    save_file.close
+    puts "Game saved!"
+    exit
+  end
+
+  def load_game
+    if (Dir.entries(@save_dir) - %w{ . .. }).empty?
+      puts "No save files found!"
+      welcome_screen
+    else
+      puts "Select which save to load: "
+      save_files = Dir.entries(@save_dir) - %w{ . .. }
+      save_files.sort!
+      save_files.each_with_index do |save, index|
+        puts "#{index + 1} - #{save}"
+      end
+      choice = gets.chomp
+      if save_files[choice.to_i - 1].nil? || choice !~ /\d+/
+        #need to check if user input is a digit
+        puts "Invalid option!"
+        load_game
+      end
+      selected_save = save_files[choice.to_i - 1]
+      saved_game = YAML::load(File.open("#{@save_dir}/#{selected_save}"))
+      saved_game.play
+    end
   end
 
   def pick_word
@@ -48,7 +106,13 @@ class Hangman
     valid = false
     until valid == true do
       input = gets.chomp.downcase
-      valid = true if input.length == 1 && input =~ /[a-z]/
+      if input.length == 1 && input =~ /[a-z]/
+        valid = true
+        return input
+      elsif input == 'save'
+        valid = true
+        save_game
+      end
     end
     return input
   end
@@ -73,4 +137,4 @@ class Hangman
   end
 end
 
-test = Hangman.new
+current_game = Hangman.new
